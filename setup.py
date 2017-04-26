132396d04746231017eb519e1136f17e621cd43c
Jonathan Wilson <wilsonwjonathan@gmail.com>
2017-04-25 22:16:15 -0700

    changed version 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ README = "README.md"
 
 setup(
     name="encryptor",
-    version='0.3',
+    version='0.4',
     description='AWS File Encryptor',
     url='https://github.com/jonwils24/encryptor',
     author='Jonathan Wilson',
